c36b7cfc260054d352167fc0f0037ff7839d6d91
Macrichie <makanjuolakunle@gmail.com>
2018-01-15 15:36:11 +0100

    Added second dice display

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ function init() {
     activePlayer = 0;
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').hidden;
+    document.getElementById('dice-2').hidden;
+
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
@@ -37,12 +39,18 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', ()=> {
     if(gamePlaying) {
         //1. Get random number
-        let dice = Math.floor(Math.random()*6) + 1;
+        let dice1 = Math.floor(Math.random()*6) + 1;
+        let dice2 = Math.floor(Math.random()*6) + 1;
 
         //2. Display the result
-        let diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        // let diceDOM = document.querySelector('.dice');
+        // diceDOM.style.display = 'block';
+        // diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
 
         //3. Update the round score IF the rolled number is NOT equal to 1
         if(dice === 6 && lastDice === 6) {
@@ -93,7 +101,9 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
 
         if(scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-            document.querySelector('.dice').hidden = true;
+
+            document.getElementById('dice-1').hidden = true;
+            document.getElementById('dice-2').hidden = true;
 
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -122,7 +132,8 @@ function nextPlayer() {
         //document.querySelector('.player-0-panel').classList.remove('active');
         //document.querySelector('.player-1-panel').classList.add('active');
 
-        document.querySelector('.dice').hidden = true;
+        document.getElementById('dice-1').hidden = true;
+        document.getElementById('dice-2').hidden = true;
         //document.querySelector('.dice').style.display = 'none';
 }
 
